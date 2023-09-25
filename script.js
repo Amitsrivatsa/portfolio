3e2104f25aaf7dc8c6adb0f622967823f114bc0f
amitsrivatsa <as60@csgate.ucc.ie>
2023-09-25 20:19:49 +0100

    added contact form

--- script.js
+++ script.js
@@ -4,3 +4,28 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+function sendMessage(){
+    (function(){
+        emailjs.init("H1JSXP8kvzu439nEB");
+    })();
+
+    var serviceID = "service_7mmfspy";
+    var templateID = "template_ykmzc4s";
+
+    var params = {
+        sendername: document.querySelector("#name").value,
+        senderemail: document.querySelector("#email").value,
+        subject: document.querySelector("#subject").value,
+        message: document.querySelector("#message").value,
+    };
+
+    emailjs.send(serviceID,templateID,params)
+    .then( res => {
+        alert('Thank You, ' + params['sendername'] + '!, Your mail has been sent.');
+    }).catch();
+
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+}
